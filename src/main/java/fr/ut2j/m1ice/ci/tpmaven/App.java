5f2a65855af8e31353f2a693ddd67a67c70e23f3
Lino <lino-tran@hotmail.fr>
2018-10-04 14:11:10 +0200

    TP Maven

---
+++ src/main/java/fr/ut2j/m1ice/ci/tpmaven/App.java
@@ -0,0 +1,13 @@
+package fr.ut2j.m1ice.ci.tpmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
